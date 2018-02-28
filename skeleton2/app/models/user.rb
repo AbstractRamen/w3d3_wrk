fce34cbca64f01588dfb3787a3b212de119dd49a
Hsuanchen Wan <walterhwan@gmail.com>
2018-02-28 14:52:27 -0800

    Finish association

---
+++ skeleton2/app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+
+  validates :name, uniqueness: true
+
+  has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :student_id,
+    class_name: 'Enrollment'
+
+  has_many :enrolled_courses,
+    through: :enrollments,
+    source: :course
+
+
+end
